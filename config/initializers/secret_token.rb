123f94024bffaa6dcfe476f33db973f55eefa746
Xavier Shay <xavier@rhnh.net>
2011-03-07 09:12:05 +1100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedAttributesDemo::Application.config.secret_token = '8af7a45306f7f83152d4134ed64c4889221675402d1685acabf368bd057e6a0ce2904e6c299e10591de1400d83faf870c1ac83be980bc11d0aaa9df23db2377d'
